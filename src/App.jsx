d3d3ff23203c44b4b4d5fb05fff7fa63b6637bb8
Hallowdread <hallowpahe@gmail.com>
2025-01-07 11:00:15 +0100

    created and imported each components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+
 export default function App() {
   return (
     <div className="app">
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,1 +1,14 @@
-export default function TodoList() {}
+export default function TodoList() {
+  return (
+    <>
+      <div className="sort-clear">
+        <select name="sort-select" id="">
+          <option value="input">Sort By Input Order</option>
+          <option value="alphabetically">Sort Alphabetically</option>
+          <option value="completed">Sort By Completed</option>
+        </select>
+        <button className="clear-button"></button>
+      </div>
+    </>
+  );
+}
