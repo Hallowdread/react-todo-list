69bf74fef5d92ef223f62f6d38d8b73d9cd67406
Hallowdread <hallowpahe@gmail.com>
2025-01-07 11:27:17 +0100

    implemented some elements and updated the index.css file

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,3 +1,21 @@
+const initialTodos = [
+  {
+    id: 32232,
+    todo: "Wash my hair",
+    completed: false,
+  },
+  {
+    id: 27688,
+    todo: "Brush my teeth",
+    completed: false,
+  },
+  {
+    id: 78777,
+    todo: "Eat my breakfast",
+    completed: true,
+  },
+];
+
 export default function TodoList() {
   return (
     <>
@@ -7,8 +25,26 @@ export default function TodoList() {
           <option value="alphabetically">Sort Alphabetically</option>
           <option value="completed">Sort By Completed</option>
         </select>
-        <button className="clear-button"></button>
+        <button className="clear-button">Clear All</button>
       </div>
+
+      <ul className="todo-list">
+        {initialTodos.map((todo) => (
+          <Todo todo={todo} key={todo.id} />
+        ))}
+      </ul>
     </>
   );
 }
+
+const Todo = ({ todo }) => {
+  return (
+    <li className="todo-item">
+      <input type="checkbox" />
+      <span className={`todo-text ${todo.completed ? "completed" : ""}`}>
+        {todo.todo}
+      </span>
+      <button className="delete-button">Delete</button>
+    </li>
+  );
+};
